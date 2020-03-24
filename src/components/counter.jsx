c6009122a083b754ec2f55a2e4cdfcfb5aa187ed
Clare Pinder <Clare.pinder@thoughtworks.com>
2020-03-24 13:43:53 +0000

    Page has randomised picture, counter and increment button

---
+++ src/components/counter.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageURL: "https:/picsum.photos/200"
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <img src={this.state.imageURL} alt="Random pic" />
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
