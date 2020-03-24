166435595fb8f7a6491d8ebf50fda4b2d55c4310
Clare Pinder <Clare.pinder@thoughtworks.com>
2020-03-24 14:37:07 +0000

    Added Bootstrap styles. Counter now has different colours for zero and non-zero

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,16 +3,24 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageURL: "https:/picsum.photos/200"
+    imageURL: "https:/picsum.photos/300"
   };
   render() {
     return (
       <React.Fragment>
         <img src={this.state.imageURL} alt="Random pic" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <div>
+          <span className={this.getCounterClass()}>{this.formatCount()}</span>
+          <button className="btn btn-secondary btn-sm">Increment</button>
+        </div>
       </React.Fragment>
     );
+  }
+
+  getCounterClass() {
+    let classStyles = "badge m-2 badge-";
+    classStyles += this.state.count === 0 ? "warning" : "primary";
+    return classStyles;
   }
 
   formatCount() {
