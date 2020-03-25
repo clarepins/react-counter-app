b445351f5b99b946c0a07c29391db6481e173581
Clare Pinder <Clare.pinder@thoughtworks.com>
2020-03-25 15:49:45 +0000

    Added delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,20 +2,26 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   render() {
     return (
       <React.Fragment>
-        <h4>Counter #{this.props.id}</h4>
+        <h4>Counter #{this.props.counter.id}</h4>
         <div>
-          <span className={this.getCounterClass()}>{this.formatCount()}</span>
+          <span className={this.getCounterClass()}>{this.formatValue()}</span>
           <button
             onClick={() => this.handleIncrement()}
             className="btn btn-secondary btn-sm"
           >
             Increment
+          </button>
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm m-2"
+          >
+            Delete
           </button>
         </div>
       </React.Fragment>
@@ -28,11 +34,11 @@ class Counter extends Component {
 
   getCounterClass() {
     let classStyles = "badge m-2 badge-";
-    classStyles += this.state.count === 0 ? "warning" : "primary";
+    classStyles += this.state.value === 0 ? "warning" : "primary";
     return classStyles;
   }
 
-  formatCount() {
+  formatValue() {
     const { value } = this.state;
     return value === 0 ? "Zero" : value;
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,11 +4,18 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 1 },
-      { id: 2, value: 2 },
-      { id: 3, value: 3 },
-      { id: 4, value: 4 }
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
     ]
+  };
+
+  handleDelete = counterId => {
+    let counters = this.state.counters.filter(
+      counter => counter.id !== counterId
+    );
+    this.setState({ counters });
   };
 
   render() {
@@ -16,7 +23,11 @@ class Counters extends Component {
       <div>
         <img src="https:/picsum.photos/1500/200" alt="Random pic" />
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} id={counter.id} />
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
         ))}
       </div>
     );
