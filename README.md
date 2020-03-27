215d6268cf917533c7e617470382aadb72242419
Clare Pinder <Clare.pinder@thoughtworks.com>
2020-03-27 10:09:24 +0000

    Now with navBar which shows total number of counters

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-counter-app
+
+This app was based on following React Crash Course from Programming with Mosh: https://www.youtube.com/watch?v=Ke90Tje7VS0&t=7168s
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,54 +2,21 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(counter => {
-      counter.value = 0;
-      return counter;
-    });
-    this.setState({ counters });
-  };
-
-  handleIncrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(
-      counter => counter.id !== counterId
-    );
-    this.setState({ counters });
-  };
-
   render() {
+    const { onReset, onIncrement, onDelete, counters } = this.props;
+
     return (
       <div>
         <img src="https:/picsum.photos/1500/200" alt="Random pic" />
-        <button
-          onClick={this.handleReset}
-          className="btn btn-primary btn-sm m-2"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
 
-        {this.state.counters.map(counter => (
+        {counters.map(counter => (
           <Counter
             key={counter.id}
-            onIncrement={this.handleIncrement}
-            onDelete={this.handleDelete}
+            onIncrement={onIncrement}
+            onDelete={onDelete}
             counter={counter}
           />
         ))}
